7d36e03a95f7fd1b2cd46f1111aeb5f258e4a2ed
CafeyTigrillo <cafeytigrillomagic@gmail.com>
2025-02-11 20:32:08 -0500

    🐳⚙️ Add Dockerfile and GitHub Actions workflow to products-category-service

---
+++ src/main/java/com/mipagina/products_category_service/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.mipagina.products_category_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir CORS en todos los endpoints
+                        .allowedOrigins("http://ec2-13-216-183-248.compute-1.amazonaws.com:3000") // Cambia esto por la IP/DNS pública de tu frontend en AWS
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
